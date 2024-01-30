e7c596cb659ebb13d9bf38afad8d0c968f6f4de4
minyongs <stylish_c@naver.com>
2024-01-30 13:42:59 +0900

    @NotEmpty 어노테이션 추가

--- src/main/java/org/example/toyboard1/dto/BoardDto.java
+++ src/main/java/org/example/toyboard1/dto/BoardDto.java
@@ -1,9 +1,11 @@
 package org.example.toyboard1.dto;
 
-import lombok.AllArgsConstructor;
+
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.toyboard1.Entity.Board;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
 
@@ -12,8 +14,12 @@ import java.time.LocalDateTime;
 public class BoardDto {
 
     private Long id;
+
+    @NotEmpty(message = "제목을 입력하세요")
     private String title;
+    @NotEmpty(message = "내용을 입력하세요")
     private String content;
+    @NotEmpty(message = "작성자를 입력하세요")
     private String writer;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
@@ -24,6 +30,7 @@ public class BoardDto {
         board.setId(boardDto.getId());
         board.setTitle(boardDto.getTitle());
         board.setContent(boardDto.getContent());
+        board.setWriter(boardDto.getWriter());
         board.setCreatedAt(boardDto.getCreatedAt());
         board.setUpdatedAt(boardDto.getUpdatedAt());
         return board;
