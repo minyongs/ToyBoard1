2f053aa07b08e6082ae61d7cb0c36a253cba16a5
minyongs <stylish_c@naver.com>
2024-01-30 12:58:13 +0900

    boardService 수정

--- src/main/java/org/example/toyboard1/service/BoardService.java
+++ src/main/java/org/example/toyboard1/service/BoardService.java
@@ -2,10 +2,12 @@ package org.example.toyboard1.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.toyboard1.Entity.Board;
+import org.example.toyboard1.dto.BoardDto;
 import org.example.toyboard1.repository.BoardRepository;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
@@ -13,27 +15,35 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    public void register(Board board){
+    public void register(BoardDto boardDto){
+        Board board = BoardDto.toBoardEntity(boardDto);
         boardRepository.save(board);
     }
 
     //전체목록
-
-    public List<Board> showAll (){
-        return boardRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    // 리스트를 가져와서 BoardDTO 로 변환
+    public List<BoardDto> showAll (){
+        return boardRepository.findAll(Sort.by(Sort.Direction.DESC, "id"))
+                .stream()
+                .map(Board::toBoardDto)
+                .collect(Collectors.toList());
     }
 
 
 
     //디테일
-    public Board showDetail(Long id){
-        return boardRepository.findById(id).orElse(null);
+    public BoardDto showDetail(Long id){
+        return boardRepository.findById(id)
+                .map(Board::toBoardDto)
+                .orElse(null);
     }
 
 
     //업데이트
 
-    public void update(Board board){
+    public void update(BoardDto boardDto){
+
+        Board board = BoardDto.toBoardEntity(boardDto);
         boardRepository.save(board);
     }
 
