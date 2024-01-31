c55ed321c0d3f53892e3ed15810055c53486d6e5
minyongs <stylish_c@naver.com>
2024-01-31 12:53:10 +0900

    작성자 안뜨는 문제 해결

--- src/main/java/org/example/toyboard1/Entity/Board.java
+++ src/main/java/org/example/toyboard1/Entity/Board.java
@@ -6,6 +6,7 @@ import org.example.toyboard1.dto.BoardDto;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -20,6 +21,7 @@ public class Board {
     private Long id;
 
     private String title;
+    @Column
     private String content;
     private String writer;
 
@@ -31,16 +33,23 @@ public class Board {
     @CreationTimestamp
     private LocalDateTime updatedAt;
 
+
+    @OneToMany(mappedBy = "board",cascade = CascadeType.ALL)//cascade : 부모 보드가 삭제될때 그 댓글들도 모두 삭제 되어야 함
+    private List<Comment> comments;
+
     public static BoardDto toBoardDto(Board board){
       BoardDto boardDto = new BoardDto();
       boardDto.setId(board.getId());
       boardDto.setTitle(board.getTitle());
       boardDto.setContent(board.getContent());
+      boardDto.setWriter(board.getWriter());
       boardDto.setCreatedAt(board.getCreatedAt());
       boardDto.setUpdatedAt(board.getUpdatedAt());
       return boardDto;
 
 
     }
+
+
 
 }
